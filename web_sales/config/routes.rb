9c677969ee17a1987d192b3733692376f50d41c3
chiennm <ngominhchien96@gmail.com>
2019-04-25 09:42:48 +0700

    Use devise to authen user

--- web_sales/config/routes.rb
+++ web_sales/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  as :user do
+    get     "/signin",  to: "devise/sessions#new"
+    get     "/signup",  to: "devise/registrations#new"
+    delete  "/signout", to: "devise/sessions#destroy"
+  end
+
+  root "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
